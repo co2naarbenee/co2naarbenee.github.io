ca0f5b12f1711dfcb7bfd2d4b1ca4b6635375c88
xGodScent <71233972+xGodScent@users.noreply.github.com>
2020-12-10 18:20:20 +0100

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,8 +1,12 @@
 
 function SubmitEmail() {
 
-	var email = document.getElementById("email").value;
+	const fs = require('fs');
+	var path = "";
 
-	
+	var email = document.getElementById("email").value;
+	let data = "Learning how to write in a file."
+
+	fs.writeFile('Output.txt', data, (err) => { if (err) throw err; })
 
 }
