9c87e340d089fe3a5f512c11c8598ba4517d82d5
kianniek <56929026+kianniek@users.noreply.github.com>
2020-12-10 22:52:20 +0100

    update finalv1

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,22 +3,7 @@
 
 function SubmitEmail() {
 
-	var path = "../../__questions__/";
-
-	var email = document.getElementById("email").value;
-	let data = "Learning how to write in a file.";
-
-	// require this to import fs module using the require module
-	const fs = require('fs')
-
-	try {
-		// b59ef7bef5e2e674a7aed946bf202843
-	  fs.renameSync('Output.json', 'b59ef7bef5e2e674a7aed946bf202843.json')
-	  //done
-	} catch (err) {
-	  console.error(err)
-	}
-
-	// fs.writeFile(path + 'Output.txt', data,  (err) => { if (err) throw err; }  );
+	var href = "mailto:co2naarbenee@gmail.com"
+	window.location=href;
 
 }
