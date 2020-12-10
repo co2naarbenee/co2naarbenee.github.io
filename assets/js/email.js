3b22ce14f9f917f475ea441e8666166dfd1410d4
xGodScent <71233972+xGodScent@users.noreply.github.com>
2020-12-10 18:34:56 +0100

    s

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,12 +1,14 @@
+import fs from fs;
 
 function SubmitEmail() {
 
-	const fs = require('fs');
-	var path = "";
+	var path = "../../__questions__/";
 
 	var email = document.getElementById("email").value;
-	let data = "Learning how to write in a file."
+	let data = "Learning how to write in a file.";
 
-	fs.writeFile('Output.txt', data, (err) => { if (err) throw err; })
+	fs.writeFile('Output.txt', data,  (err) => { if (err) throw err; }  );
+
+
 
 }
