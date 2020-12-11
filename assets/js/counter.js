8ea3e3b010aa57a02aacd58546d22270d125dd11
kianniek <56929026+kianniek@users.noreply.github.com>
2020-12-11 11:51:23 +0100

    analics added

---
+++ assets/js/counter.js
@@ -0,0 +1,7 @@
+function counter() {
+    var views = parseInt(document.getElementById("2dccd1ab3e03990aea77359831c85ca2").textContent);
+    views++;
+    document.getElementById("2dccd1ab3e03990aea77359831c85ca2").innerHTML = views;
+    console.log(views);
+}
+counter();
